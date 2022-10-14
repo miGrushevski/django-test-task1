90f61df8ed9c516153aca55d961d12ecf1d2e3d4
Andrei Barushka <mi.grushevski@gmail.com>
2022-10-14 15:34:38 +0300

    added table with total summary per year-month

--- expenses/reports.py
+++ expenses/reports.py
@@ -1,7 +1,8 @@
 from collections import OrderedDict
+from datetime import datetime
 
 from django.db.models import Sum, Value
-from django.db.models.functions import Coalesce
+from django.db.models.functions import Coalesce, ExtractYear, ExtractMonth
 
 
 def summary_per_category(queryset):
@@ -19,3 +20,19 @@ def get_total_spent_amount(queryset):
     return queryset.aggregate(Sum('amount'))['amount__sum']
 
 
+def get_summary_per_year_month(queryset):
+    years_months = OrderedDict()
+    years = sorted(set(map(lambda x: x['year'], queryset
+                           .annotate(year=ExtractYear('date')).values('year'))))
+    for year in years:
+        months = sorted(set(map(lambda x: x['month'], queryset
+                                .filter(date__year=year)
+                                .annotate(month=ExtractMonth('date'))
+                                .values('month'))))
+        for month in months:
+            sum_month = \
+                queryset.filter(date__year=year, date__month=month).aggregate(Sum('amount'))['amount__sum']
+            datetime_object = datetime.strptime(str(month), "%m")
+            month_name = datetime_object.strftime("%b")
+            years_months[f'{year}-{month_name}'] = sum_month
+    return years_months
--- expenses/views.py
+++ expenses/views.py
@@ -2,7 +2,7 @@ from django.views.generic.list import ListView
 
 from .forms import ExpenseSearchForm
 from .models import Expense, Category
-from .reports import summary_per_category, get_total_spent_amount
+from .reports import summary_per_category, get_total_spent_amount, get_summary_per_year_month
 
 
 class ExpenseListView(ListView):
@@ -38,6 +38,7 @@ class ExpenseListView(ListView):
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
             total_spent_amount=get_total_spent_amount(queryset),
+            total_spent_per_year_month=get_summary_per_year_month(queryset),
             **kwargs)
 
 class CategoryListView(ListView):
